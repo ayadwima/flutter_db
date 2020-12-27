7cdbd340a2aa4a88f6c47e4b08120a70e8c4311c
ayadwima <ayoshdwima610@gmail.com>
2020-12-27 23:49:00 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_sql/Provider/app_provider.dart';
 import 'screens/FirstScreen.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-    create: (context) {
-      return AppProvider();
-    },
-    child: MaterialApp(
+ MaterialApp(
       debugShowCheckedModeBanner: false,
       home: TodoPage(),
     ),
-  ));
+  );
 }
